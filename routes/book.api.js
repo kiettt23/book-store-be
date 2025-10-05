d84e60624f2d88459004f5e8ecf57060467ab6db
kiettt23 <kiettt23@gmail.com>
2025-10-05 15:32:13 +0700

    feat(books): add POST endpoint to create a new book

--- routes/book.api.js
+++ routes/book.api.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const fs = require("fs");
 const express = require("express");
 const router = express.Router();
@@ -55,11 +56,69 @@ router.get("/", (req, res, next) => {
     } else {
       result = books;
     }
-    
+
     //then select number of result by offset
     result = result.slice(offset, offset + limit);
     //send response
     res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * params: /
+ * description: post a book
+ * query:
+ * method: post
+ */
+
+router.post("/", (req, res, next) => {
+  //post input validation
+  try {
+    const { author, country, imageLink, language, pages, title, year } =
+      req.body;
+    if (
+      !author ||
+      !country ||
+      !imageLink ||
+      !language ||
+      !pages ||
+      !title ||
+      !year
+    ) {
+      const exception = new Error(`Missing body info`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+
+    //post processing
+    const newBook = {
+      author,
+      country,
+      imageLink,
+      language,
+      pages: parseInt(pages) || 1,
+      title,
+      year: parseInt(year) || 0,
+      id: crypto.randomBytes(4).toString("hex"),
+    };
+    //Read data from db.json then parse to JSobject
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { books } = db;
+
+    //Add new book to book JS object
+    books.push(newBook);
+    //Add new book to db JS object
+    db.books = books;
+    //db JSobject to JSON string
+    db = JSON.stringify(db);
+    //write and save to db.json
+    fs.writeFileSync("db.json", db);
+
+    //post send
+    res.status(200).send(newBook);
   } catch (error) {
     next(error);
   }

40dcc041b8b8bd3db7742a752dbdd492d270a990
kiettt23 <kiettt23@gmail.com>
2025-10-05 15:35:22 +0700

    feat(books): implement update book by id endpoint

--- routes/book.api.js
+++ routes/book.api.js
@@ -56,9 +56,9 @@ router.get("/", (req, res, next) => {
     } else {
       result = books;
     }
-
     //then select number of result by offset
     result = result.slice(offset, offset + limit);
+
     //send response
     res.status(200).send(result);
   } catch (error) {
@@ -119,6 +119,68 @@ router.post("/", (req, res, next) => {
 
     //post send
     res.status(200).send(newBook);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * params: /
+ * description: update a book
+ * query:
+ * method: put
+ */
+
+router.put("/:bookId", (req, res, next) => {
+  //put input validation
+  try {
+    const allowUpdate = [
+      "author",
+      "country",
+      "imageLink",
+      "language",
+      "pages",
+      "title",
+      "year",
+    ];
+
+    const { bookId } = req.params;
+
+    const updates = req.body;
+    const updateKeys = Object.keys(updates);
+    //find update request that not allow
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+
+    //put processing
+    //Read data from db.json then parse to JSobject
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { books } = db;
+    //find book by id
+    const targetIndex = books.findIndex((book) => book.id === bookId);
+    if (targetIndex < 0) {
+      const exception = new Error(`Book not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+    //Update new content to db book JS object
+    const updatedBook = { ...db.books[targetIndex], ...updates };
+    db.books[targetIndex] = updatedBook;
+
+    //db JSobject to JSON string
+
+    db = JSON.stringify(db);
+    //write and save to db.json
+    fs.writeFileSync("db.json", db);
+
+    //put send response
+    res.status(200).send(updatedBook);
   } catch (error) {
     next(error);
   }
